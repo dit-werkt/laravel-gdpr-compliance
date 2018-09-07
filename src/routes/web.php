aa2a3535def08a027f07abe58cb30978cb1ad713
Daniel Billing <daniel@infihex.com>
2018-09-07 22:13:55 +0200

    Make the URIs beatiful

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,12 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('download', 'GdprController@download');
 
-Route::get('/show_terms', 'GdprController@showTerms');
-Route::post('terms_accepted', [
+Route::get('/terms', 'GdprController@showTerms');
+Route::post('terms/accepted', [
     'as' => 'terms_accepted',
     'uses' => 'GdprController@termsAccepted',
 ]);
-Route::post('terms_denied', [
+Route::post('terms/denied', [
     'as' => 'terms_denied',
     'uses' => 'GdprController@termsDenied',
 ]);
