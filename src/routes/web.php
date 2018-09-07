a2dc2bda03945eefb62ca7a2d2f9f1e1969517b3
Daniel Billing <daniel@infihex.com>
2018-09-07 22:16:51 +0200

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::post('download', 'GdprController@download');
 
 Route::get('/terms', [
-    'as' => 'terms',
+    'as' => 'gdpr-terms',
     'uses' => 'GdprController@showTerms',
 ]);
 Route::post('terms/accepted', [
-    'as' => 'terms_accepted',
+    'as' => 'gdpr-terms_accepted',
     'uses' => 'GdprController@termsAccepted',
 ]);
 Route::post('terms/denied', [
-    'as' => 'terms_denied',
+    'as' => 'gdpr-terms_denied',
     'uses' => 'GdprController@termsDenied',
 ]);
